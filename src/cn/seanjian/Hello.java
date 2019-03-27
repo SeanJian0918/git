848cc432963c472af5765ffa04fd16bd1de9ff2b
seanjian <seanjian>
2019-03-27 13:19:39 +0800

    大事发生

---
+++ src/cn/seanjian/Hello.java
@@ -0,0 +1,16 @@
+package cn.seanjian;
+
+/**
+ * @Auther: Sean Jian
+ * @Description: cn.seanjian
+ * @Date: 2019/3/27 12:54
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("w23322法第三方三");
+        System.out.println("w23weewdfsafs32");
+        System.out.println("dfsdfsf");
+
+    }
+}
